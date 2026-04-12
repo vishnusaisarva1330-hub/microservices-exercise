1c47cf3043a838238b0a144ec4f033b833b7588f
Vishnu Sai Sarva <vishnusaisarva1330@gmail.com>
2026-04-12 11:45:46 -0400

    1A-setup

---
+++ product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
